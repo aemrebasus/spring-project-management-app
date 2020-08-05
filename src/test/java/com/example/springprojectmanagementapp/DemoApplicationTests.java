96532efe8d069889aa55bc0ec5804c8093433747
Ahmet Emrebas <aemrebasus@gmail.com>
2020-08-05 04:12:35 -0500

    init the project

---
+++ src/test/java/com/example/springprojectmanagementapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojectmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
