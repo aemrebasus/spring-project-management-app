99c815bd0f15b1d64efbfd5837c881dfd8db3045
Ahmet Emrebas <aemrebasus@gmail.com>
2020-08-06 08:02:51 -0500

    Project Service implemented (Not related closes #8)

--- src/main/java/com/aemrebas/springprojectmanagementapp/services/ProjectService.java
+++ src/main/java/com/aemrebas/springprojectmanagementapp/services/ProjectService.java
@@ -1,22 +1,20 @@
-package com.aemrebas.springprojectmanagementapp.repositories;
+package com.aemrebas.springprojectmanagementapp.services;
 /*
- @author Ahmet Emrebas on 8/5/2020
  @project spring-project-management-app
- @since 1.0.0
+ @author Ahmet Emrebas on 8/6/2020
+ @since 1.0.0 
 */
 
 import com.aemrebas.springprojectmanagementapp.domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectService extends IService<Project> {
     List<Project> findAllProjectsByOrganizationId(Long id);
 
     List<Project> findAllProjectsByDescriptionContains(String description);
 
     Project findOneProjectByName(String name);
+
     Project findOneProjectByNameContains(String name);
-
-
 }
---
+++ src/main/java/com/aemrebas/springprojectmanagementapp/services/ProjectServiceImp.java
@@ -0,0 +1,68 @@
+package com.aemrebas.springprojectmanagementapp.services;
+/*
+ @project spring-project-management-app
+ @author Ahmet Emrebas on 8/6/2020
+ @since 1.0.0 
+*/
+
+import com.aemrebas.springprojectmanagementapp.domain.Project;
+import com.aemrebas.springprojectmanagementapp.repositories.ProjectRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service("projectService")
+public class ProjectServiceImp implements ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Override
+    public List<Project> findAll() {
+        return projectRepository.findAll();
+    }
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        return projectRepository.findById(id);
+    }
+
+    @Override
+    public void saveOne(Project entity) {
+        projectRepository.save(entity);
+    }
+
+    @Override
+    public void updateOneById(Long id, Project entity) {
+        Project existing = projectRepository.getOne(id);
+        BeanUtils.copyProperties(entity, existing, "id");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        projectRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Project> findAllProjectsByOrganizationId(Long id) {
+        return projectRepository.findAllProjectsByOrganizationId(id);
+    }
+
+    @Override
+    public List<Project> findAllProjectsByDescriptionContains(String description) {
+        return projectRepository.findAllProjectsByDescriptionContains(description);
+    }
+
+    @Override
+    public Project findOneProjectByName(String name) {
+        return projectRepository.findOneProjectByName(name);
+    }
+
+    @Override
+    public Project findOneProjectByNameContains(String name) {
+        return projectRepository.findOneProjectByNameContains(name);
+    }
+}
